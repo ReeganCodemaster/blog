90de1725c3ab412deb3396020cf777bda12a9514
ReeganCodemaster <reeganviljoen@outlook.com>
2022-04-02 12:12:07 +0200

    Added realtionship to comments, created partial comments view and partial comments form

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_02_093259) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_02_100352) do
   create_table "articles", force: :cascade do |t|
     t.text "title"
     t.string "body"
@@ -23,6 +23,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_02_093259) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "article_id", null: false
+    t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
+  add_foreign_key "comments", "articles"
 end
